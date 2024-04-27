1feabd8930ef806c8e813d6c3fddfa0d8d0a8bbc
r4g3r4hk <166988972+r4g3r4hk@users.noreply.github.com>
2024-04-27 16:02:33 +0800

    Update battlesys.cpp

--- battlesys.cpp
+++ battlesys.cpp
@@ -176,3 +176,181 @@ void gamelevel(vector<string>& names, vector<int>& health, vector<int>& attack, 
         sleep(2);
         cout << "There's a long road ahead!" << endl;
         sleep(2);
+        break;
+      case 4:
+        cout << "You're halfway there!" << endl;
+        sleep(2);
+        cout << "You must defeat all the dragons! Push on!" << endl;
+        sleep(2);
+        break;
+      case 5:
+        cout << "You got this, warrior!" << endl;
+        sleep(2);
+        cout << "You mustn't lose now!" << endl;
+        sleep(2);
+        break;
+      case 6:
+        cout << "You're almost at the final stretch!" << endl;
+        sleep(2);
+        cout << "Don't you give in now!" << endl;
+        sleep(2);
+        break;
+      case 7:
+        cout << "This is the last fight." << endl;
+        sleep(2);
+        cout << "You've gathered all this strength," << endl;
+        sleep(2);
+        cout << "and now the only thing standing between you and victory," << endl;
+        sleep(2);
+        cout << "is this final boss." << endl;
+        sleep(2);
+        cout << "So give it your all," << endl;
+        sleep(2);
+        cout << "and go give 'em hell!" << endl;
+        sleep(2);
+        break;
+     }
+    screen_clear();
+    
+  //battle choice
+    while (health[0] > 0 && health[i] > 0) {
+      int dragondamage = 0, protection = 0, playerdamage = 0, blockeddamage = 0, moneygain = 0, managain = 0;
+      bool attacking = false;
+      printgamingstats(names, health, attack, defense, mana, difficulty, picture, money, i, tempstring, tempdh);
+      while (attacking == false){
+        
+        char choice;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        //attacking make damge with range, it can be reduced by dragon defense. earning money and mana)
+        switch (choice) {
+        case 'a':
+        case 'A':
+          dragondamage = rdgen(attack[0]/1.6, attack[0]*1.6);
+          if (dragondamage >= defense[i])
+            dragondamage -= defense[i];
+          else 
+            dragondamage = 0;
+          managain = rdgen(2, 10);
+          mana[0] += managain;
+          moneygain = rdgen(7, 20);
+          money += moneygain;
+          attacking = true;
+          break;
+
+        //smite make a solid damge, cant be reduced. earn money only but 2x.
+        case 'S':
+        case 's':
+          if (mana[0] >= 20) {
+              mana[0] -= 20;
+              dragondamage = attack[0]*2.2;
+              moneygain = rdgen(20, 35);
+              money += moneygain;
+              attacking = true;
+              break;
+          }
+          else {
+            screen_clear();
+            cout << "-------------------------------------" << endl <<"You need more \033[34mMana\033[0m!" << endl << "------------------------------------" << endl;
+            sleep(1);
+            screen_clear();
+            printgamingstats(names, health, attack, defense, mana, difficulty, picture, money, i, tempstring, tempdh);
+            break;
+          }
+
+          //make zero damge to dragon by defensing yourself with range, only earned mana but.
+        case 'd':
+        case 'D':
+          protection = rdgen(defense[0]/1.6, defense[0]*1.6);
+          managain = rdgen(2, 10);
+          mana[0] += managain;
+          attacking = true;
+          break;
+
+          //go shopping
+        case 'P':
+        case 'p':
+          screen_clear();
+          printstore(health, attack, defense, money, i, tempstring);
+          screen_clear();
+          printgamingstats(names, health, attack, defense, mana, difficulty, picture, money, i, tempstring, tempdh);
+          break;
+          
+        default:
+          screen_clear();
+          cout << "-------------------------------------" << endl <<"\033[1;31mInvalid\033[0m choice. Please try again." << endl << "------------------------------------" << endl;
+          sleep(1);
+          screen_clear();
+          printgamingstats(names, health, attack, defense, mana, difficulty, picture, money, i, tempstring, tempdh);
+        } 
+      }
+
+      //damage system
+      playerdamage = (rdgen(attack[i]/1.7, attack[i]*1.7));
+      if (protection >= playerdamage)
+        blockeddamage = 0;
+      else 
+        blockeddamage = playerdamage - protection;
+      health[i] -= dragondamage;
+      
+
+      //print damage
+      if (health[i] <= 0) {
+        tempstring.push_back("Killed level " + to_string(i) + " dragon!"+ " | " + "Money earned: " + to_string(moneygain) + " | " + "Mana gained: " + to_string(managain));
+        tempstring.erase(tempstring.begin());
+        screen_clear();
+        cout << "--------------------------------------------------------------------------" << endl << " You killed the dragon with \033[1;31m" + to_string(dragondamage) + "\033[0m damage!" << endl << "-------------------------------------------------------------------------" << endl;
+        sleep(2);
+        screen_clear();
+        }
+      else {
+        health[0] -= blockeddamage;
+        tempstring.push_back("Blocked: " + to_string(protection) + " damage" + " | " + "Dealt: " + to_string(dragondamage) + " damage" + " | " + "Enemy dealt: " + to_string(playerdamage) + " damage" + " | " + "Money earned: " + to_string(moneygain) + " | " + "Mana gained: " + to_string(managain));
+        tempstring.erase(tempstring.begin());
+        screen_clear();
+        cout << "--------------------------------------------------------------------------" << endl <<"You deal \033[1;31m" + to_string(dragondamage) + "\033[0m damage to " + names[i] + " but it also hit you with \033[1;31m" + to_string(blockeddamage) + "\033[0m damage!!!" << endl << "-------------------------------------------------------------------------" << endl;
+        sleep(3);
+        screen_clear();
+      }
+    }
+  //game over
+  if (health[0] <= 0){
+      char choice1;
+      bool loop3 = true;
+          while (loop3 == true) {
+              cout << "\033[1;31mYou died!\033[0m" << endl;
+              cout << "\033[32m-------------------------\033[0m" << endl;
+              cout << "R - Restart" << endl;
+              cout << "Q - Quit" << endl;
+              cout << "\033[32m-------------------------\033[0m" << endl;
+              cout << "Enter your choice: ";
+              cin >> choice1;
+
+              switch (choice1) {
+                  case 'R':
+                  case 'r':
+                      health[0] = 100;
+                      gamelevel(names, health, attack, defense, mana, difficulty, picture, money);
+                      break;
+                  case 'Q':
+                  case 'q':
+                      loop3 = false;
+                      break;
+                  default:
+                      screen_clear();
+                      cout << "-------------------------------------" << endl <<"Invalid choice. Please try again." << endl << "------------------------------------" << endl;
+                      sleep(1);
+                      screen_clear();
+              }
+          }          
+      gameover();
+      break;
+      }  
+    
+  else if (i == 7 && health[0] > 0){
+      gamewin();
+      break;
+      }  
+  }
+}

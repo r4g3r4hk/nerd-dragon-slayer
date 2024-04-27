5e668d8abf266a45d32eb0a6f89ecbe987d8cd94
EricMingChit <158144260+EricMingChit@users.noreply.github.com>
2024-04-27 08:40:02 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <unistd.h>
+#include <string>
+#include "uti.h"
+#include "picture.h"
+#include "battlesys.h"
+using namespace std;
+
+//store player and level stats
+vector<string> names;
+vector<int> health;
+vector<int> attack;
+vector<int> defense;
+vector<int> mana;
+vector<string> difficulty;
+vector<string> picture;
+int money = 100;
+
+//starting screen after pressing S
+void PlayerBuildScreen(){
+  string playername;
+  screen_clear();
+  cout << "What is your name, warrior? (under 20 letters): ";
+  cin >> playername;
+  //replace the vector with the player name
+  names[0] = playername;
+  cout << "\033[1;31;40m" << picture[17] << "\033[0m" << endl;
+  sleep(1);
+  cout << "\033[1m" << names[0] << ", these are your starting stats.\033[0m" << endl;
+  sleep(1);
+  cout << "Health: " << health[0] << endl;
+  sleep(1);
+  cout << "Attack: " << attack[0] << endl;
+  sleep(1);
+  cout << "Defense: " << defense[0] << endl;
+  sleep(1);
+  cout << "Mana: " << mana[0] << endl;
+  sleep(1);
+  cout << "You will soon fight your first enemy, are you ready?" << endl;
+  sleep(2);
+  screen_clear();
+  for (int i = 0; i < 3; ++i) {
+    usleep(500000);
+    cout << "Loading..." << endl;
+  }
+  gamelevel(names, health, attack, defense, mana, difficulty, picture, money);
+}
+
+//Rule screen after pressing R
+void Rules_Screen(){
+  screen_clear();
+
+  char choice3;
+  bool loop2 = true;
+    while (loop2 == true) {
+      cout << "\033[1;31;40m" << picture[16] << "\033[0m" << endl;
+      cout << "\033[1mBackground: \033[0m" << endl;
+      cout << "You are a game developer who fell asleep while coding his game, Nerd Dragon Slayer, and wakes up trapped inside it." << endl;
+      cout << "Without a functioning quit feature, you must defeat the dragon within the game to escape. "<< endl;
+      cout << "\033[0mThere are only two ways to end this game, either by defeating the dragons, or by \033[1,31mdying.\033[0m" << endl << endl;
+
+      cout << "\033[1mRules: \033[0m" << endl;
+      cout << "The player starts with initial values for \033[1mHealth, Attack, Defense, Mana, and Money.\033[0m" << endl;
+      cout << "These attributes define the player's capabilities within the game." << endl;
+      cout << "You can choose to increase your \033[1mAttack, Defense\033[0m or heal yourself by spending money in the shop." << endl;
+      cout << "You will have 3 options to fight the dragon, \033[1mAttack, Defend, and Smite.\033[0m" << endl;
+      cout << "  1. \033[1,31mAttack\033[0m: You will attack the dragon, dealing damage to it based on your attack stat. (+Money, +Mana)" << endl;
+      cout << "  2. \033[1,31mDefend\033[0m: You will defend against the dragon's attack, reducing the damage based on your defense stat. (+Mana)" << endl; 
+      cout << "  3. \033[1,31mSmite\033[0m: You will attack the dragon with fixed 2.2x damage, consuming mana in the process. (+ Extra Money)" << endl;
+      cout << "During each round, the dragon will also attack the player and your health will decrease accordingly. You must manage your resources and abilities strategically to survive and defeat the dragons. \033[1:33mGood luck, warrior!\033[0m" << endl;
+      // rule screen menu

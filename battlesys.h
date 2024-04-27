e408fc6f94ef59ca3ac73fa40d4a8382a4385361
r4g3r4hk <166988972+r4g3r4hk@users.noreply.github.com>
2024-04-27 16:03:15 +0800

    Update battlesys.h

--- battlesys.h
+++ battlesys.h
@@ -4,3 +4,7 @@
 #include <vector>
 #include <string>
 using namespace std;
+
+void gamelevel(vector<string>& names, vector<int>& health, vector<int>& attack, vector<int>& defense, vector<int>& mana, vector<string>& difficulty, vector<string>& picture, int money);
+
+#endif

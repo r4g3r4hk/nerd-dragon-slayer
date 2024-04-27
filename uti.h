dacf6fb0daa4982dc2c14809f51fc0059e40d09c
r4g3r4hk <166988972+r4g3r4hk@users.noreply.github.com>
2024-04-27 16:06:34 +0800

    Update uti.h

--- uti.h
+++ uti.h
@@ -4,3 +4,10 @@
 #include <vector>
 #include <string>
 using namespace std;
+
+void screen_clear();
+void readStatsFromFile(vector<string>& names, vector<int>& health, vector<int>& attack, vector<int>& defense, vector<int>& mana, vector<string>& difficulty);
+void gamewin();
+void gameover();
+
+#endif
